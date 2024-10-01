96544532bf63cad4e62a8b5cac0d9e349da9fe82
joaomonsy <joaomonsinhatti@gmail.com>
2024-10-01 17:42:50 -0300

    Tarefa Atualizada

--- app/src/main/java/com/example/appfirebase/MainActivity.kt
+++ app/src/main/java/com/example/appfirebase/MainActivity.kt
@@ -6,14 +6,18 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,7 +29,11 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.appfirebase.ui.theme.AppFirebaseTheme
 import com.google.firebase.firestore.FirebaseFirestore
@@ -70,7 +78,27 @@ fun App(db: FirebaseFirestore) {
                 .fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
         ) {
-            Text(text = "App Firebase Firestore")
+            // Image with rounded borders and smaller size
+            Image(
+                painter = painterResource(id = R.drawable.palmeiras),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .border(2.dp, Color.Gray, RoundedCornerShape(8.dp))
+                    .padding(8.dp)
+                    .size(100.dp)  // Adjust the size of the image
+            )
+        }
+
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = "João Lucas Monsynhatti Lima")
+                Text(text = "3 DS Manhã")
+            }
         }
 
         Row(
